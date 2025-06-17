1f1c4108c945893833b0e5a7dc6750d1a8f33a34
AnandaSubramanianM <anandsubramanian217@gmail.com>
2025-06-17 21:35:50 +0530

    Reverse String with for loop

--- src/main/java/universe/Practice.java
+++ src/main/java/universe/Practice.java
@@ -4,7 +4,15 @@ public class Practice {
 	
 	public static void main (String args[])
 	{
-		System.out.println("Hello world");
+		System.out.println("Hello world  " +stringReverse("Hello world"));
+	}
+	
+	//Write a Java Program to reverse a string without using String inbuilt function.
+	public static String stringReverse(String value)
+	{
+		String reverse="";
+		for(int i=1 ;i<=value.length();i++) reverse +=value.charAt(value.length()-i);
+		return reverse;
 	}
 
 }
